2948c47617dad87a3a55e4de7027f1d546c51a92
Vishesh Sood <omg.itsvishesh@gmail.com>
2017-05-15 17:46:58 -0700

    job description

--- BuildAResume/src/backend/services/job-description-keywords/crawl.py
+++ BuildAResume/src/backend/services/job-description-keywords/crawl.py
@@ -3,9 +3,7 @@ import urllib
 from bs4 import BeautifulSoup
 import sys
 
-r = urllib.urlopen(sys.argv[1]).read()
 
-soup = BeautifulSoup(r,"html.parser")
 
 words = ["Agile","Applications","ASP.NET","Build","C#","HTML","CSS","Mongodb","AngularJS","NodeJS",
 "ExpressJS","MEAN","ASP.NET","MySQL","Oracle","ADO.NET","Script", "Code","CoffeeScript","TypeScript", "Ruby","Python",
@@ -14,20 +12,35 @@ words = ["Agile","Applications","ASP.NET","Build","C#","HTML","CSS","Mongodb","A
 "Testing","Tools","Bootsrap","C++","C","Web", "iOS", "JS", "Android", "HTML5", "CSS3", "SQL", 
 "Swift", "React", "Redux", "PHP", "Scala"]
 
-list_tags = soup.findAll('li')
-words = [item.lower() for item in words]
-output = ""
-for a in list_tags:
-	text = a.text
-	textList = text.split(" ")
-	
-	for one in textList:
-		
+source = sys.argv[1];
+
+if 'http' in source:
+	r = urllib.urlopen(sys.argv[1]).read()
+	soup = BeautifulSoup(r,"html.parser")
+
+
+	list_tags = soup.findAll('li')
+	words = [item.lower() for item in words]
+	output = ""
+	for a in list_tags:
+		text = a.text
+		textList = text.split(" ")		
+		for one in textList:	
+			regex = re.compile('[^a-zA-Z]')
+			#First parameter is the replacement, second parameter is your input string
+			one = regex.sub('', one)
+			if one.lower() in words:
+				output += " " + one.lower()
+	print output
+else :
+	list_of_words = source.split()
+	words = [item.lower() for item in words]
+	output = ""
+	for a in list_of_words:
 		regex = re.compile('[^a-zA-Z]')
 		#First parameter is the replacement, second parameter is your input string
-		one = regex.sub('', one)
-		if one.lower() in words:
-			output += " " + one.lower()
-
-print output
+		a = regex.sub('', a)
+		if a.lower() in words:
+			output += " " + a.lower()
+	print output
 
